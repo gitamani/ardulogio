571bed7becc41f0d8dee964acfe023864501bf01
Giuseppe Tamanini <32442678+gitamani@users.noreply.github.com>
2018-02-23 00:58:47 +0100

    Update ESP-12EServerMega.ino

--- ESP-12EServerMega.ino
+++ ESP-12EServerMega.ino
@@ -1,84 +1,50 @@
 //Creato da Giuseppe Tamanini
 //20-02-2018
 //con licenza CC BY-SA
- 
+
+#include <SPI.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
 
-unsigned long ttime;
-boolean s_time = true;
-const char* ssid = "retewifi";
-const char* password = "password";
- 
-int ledPin = 5;
-WiFiServer server(80);
-IPAddress ip(192, 168, 1, 69); // where xx is the desired IP Address
-IPAddress gateway(192, 168, 1, 1); // set gateway to match your network
+char ssid[] = "Nomeretewifi";               // SSID della wifi del tuo router
+char pass[] = "passwordretewifi";          // password del wifi del tuo router
+WiFiServer server(80);                    
 
+IPAddress ip(192, 168, 1, 80);            // indirizzo IP da assegnareal Server
+IPAddress gateway(192,168,1,1);           // Gateway del router
+IPAddress subnet(255,255,255,0);          // Subnet Mask del tuo router
 void setup() {
-  Serial.begin(9600);
-  delay(10);
- 
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
- 
-  //Serial.print(F("Setting static ip to : "));
-  //Serial.println(ip);
- 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
-  WiFi.config(ip, gateway, subnet); 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL : ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
- 
-}
- 
-void loop() {
-  if (s_time==true) {
-    ttime=millis();
-    s_time=false;
-  }
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
- 
-  // Wait until the client sends some data
-  //Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
- 
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
   
-  // Return the response
-  if (millis()-ttime>=120000) {
-    client.println("send");
-    delay(1);
+  Serial.begin(115200);                   // solo per il debug
+  WiFi.config(ip, gateway, subnet);       // configura l'IP statico
+  WiFi.begin(ssid, pass);                 // connette la wifi al router
+  while (WiFi.status() != WL_CONNECTED) { // attende che la connesione sia avvenuta
+    Serial.print(".");
+    delay(500);
+  }
+  server.begin();                         // Avvia il Server
+  /*Serial.println("Connected to wifi");
+  Serial.print("Status: "); Serial.println(WiFi.status());  // Stampa i parametri della connessione
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
+  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());*/
+  
+}
+
+void loop () {
+  WiFiClient client = server.available(); // attende un client
+  if (client) {
+    if (client.connected()) { // se un client è connesso
+      //Serial.println(".");
+      String request = client.readStringUntil('\r');    // Riceve il messaggio dal client
+      //Serial.print("Dal Client: ");
+      Serial.println(request); // manda sulla seriale (dell'Arduino) il messaggio ricevuto
+      client.flush(); // attende finché non sono stati inviati tutti i caratteri in uscita
+      client.print(request);
+      client.println("\r"); // rimanda il messaggio al client
+    }
+    client.stop();  // termina la connessione con il client
   }
 }
